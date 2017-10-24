69da2cd713c1eb8e0d78c38a68221b716f2ce911
Chad Duffey <chadduffey@hotmail.com>
2017-10-24 08:16:39 -0700

    socket framework set up

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # socketio_testing
 trying out socketIO for flask
+
+using this tutorial: https://www.shanelynn.ie/asynchronous-updates-to-a-webpage-with-flask-and-socket-io/
+
+
--- app.py
+++ app.py
@@ -1,9 +1,60 @@
-from flask import Flask
+
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+from random import random
+from time import sleep
+from threading import Thread, Event
+
+
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    return "Hello World!"
- 
-if __name__ == "__main__":
-    app.run()
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+
+#turn the flask app into a socketio app
+socketio = SocketIO(app)
+
+
+#random number Generator Thread
+thread = Thread()
+thread_stop_event = Event()
+
+class RandomThread(Thread):
+    def __init__(self):
+        self.delay = 1
+        super(RandomThread, self).__init__()
+
+    def randomNumberGenerator(self):
+
+        print "Generating random numbers"
+        while not thread_stop_event.isSet():
+            number = round(random()*10, 3)
+            print number
+            socketio.emit('newnumber', {'number': number}, namespace='/test')
+            sleep(self.delay)
+
+    def run(self):
+        self.randomNumberGenerator()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    print('Client connected')
+
+    if not thread.isAlive():
+        print "Starting Thread"
+        thread = RandomThread()
+        thread.start()
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
